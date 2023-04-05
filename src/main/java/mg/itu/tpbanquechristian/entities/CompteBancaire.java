e170138d15a903b9d85e561b5167ad2e20472d67
Stéphan Christian <schristian@bocasay.com>
2023-04-05 15:29:08 +0300

    Écriture d'une entité CompteBancaire

--- src/main/java/mg/itu/tpbanquechristian/entities/CompteBancaire.java
+++ src/main/java/mg/itu/tpbanquechristian/entities/CompteBancaire.java
@@ -21,6 +21,24 @@ public class CompteBancaire implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String nom;
+    private int solde;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getSolde() {
+        return solde;
+    }
+
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
 
     public Long getId() {
         return id;
@@ -50,5 +68,22 @@ public class CompteBancaire implements Serializable {
     public String toString() {
         return "mg.itu.tpbanquechristian.entities.CompteBancaire[ id=" + id + " ]";
     }
-    
+
+    public CompteBancaire(String nom, int solde) {
+        this.nom = nom;
+        this.solde = solde;
+    }
+
+    public void deposer(int montant) {
+        solde += montant;
+    }
+
+    public void retirer(int montant) {
+        if (montant < solde) {
+            solde -= montant;
+        } else {
+            solde = 0;
+        }
+    }
+
 }
